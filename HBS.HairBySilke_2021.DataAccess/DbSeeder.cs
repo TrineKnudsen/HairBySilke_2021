2fd9a07317766b00faf744149b31a97da3e2a068
Minaac-ops <72027505+Minaac-ops@users.noreply.github.com>
2021-12-20 03:36:29 +0100

    retter

--- HBS.HairBySilke_2021.DataAccess/DbSeeder.cs
+++ HBS.HairBySilke_2021.DataAccess/DbSeeder.cs
@@ -116,41 +116,41 @@ namespace HBS.HairBySilke_2021.DataAccess
         {
             var treatmentEntities = new List<TreatmentEntity>();
 
-            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Dameklip", Price = 325, Duration = 45});
-            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Bundfarve", Price = 415, Duration = 90});
-            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Helfarve", Price = 500, Duration = 100});
+            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Dameklip", Price = 325, Duration = 60});
+            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Bundfarve", Price = 415, Duration = 120});
+            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Helfarve", Price = 500, Duration = 120});
             treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Balayage", Price = 875, Duration = 120});
             treatmentEntities.Add(new TreatmentEntity
-                {TreatmentName = "Balayage + Bundfarve", Price = 1075, Duration = 180});
+                {TreatmentName = "Balayage + Bundfarve", Price = 1075, Duration = 120});
             treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Toning", Price = 250, Duration = 45});
             treatmentEntities.Add(new TreatmentEntity
                 {TreatmentName = "Striber - kort hår", Price = 420, Duration = 120});
             treatmentEntities.Add(new TreatmentEntity
-                {TreatmentName = "Striber - Skulderlangt hår", Price = 630, Duration = 150});
+                {TreatmentName = "Striber - Skulderlangt hår", Price = 630, Duration = 120});
             treatmentEntities.Add(new TreatmentEntity
-                {TreatmentName = "Striber - Langt hår", Price = 775, Duration = 150});
+                {TreatmentName = "Striber - Langt hår", Price = 775, Duration = 120});
             treatmentEntities.Add(new TreatmentEntity
-                {TreatmentName = "Permanent - kort hår", Price = 575, Duration = 150});
+                {TreatmentName = "Permanent - kort hår", Price = 575, Duration = 120});
             treatmentEntities.Add(new TreatmentEntity
                 {TreatmentName = "Permanent - Skulderlangt hår", Price = 755, Duration = 120});
             treatmentEntities.Add(new TreatmentEntity
-                {TreatmentName = "Permanent - Langt hår", Price = 875, Duration = 150});
+                {TreatmentName = "Permanent - Langt hår", Price = 875, Duration = 120});
             treatmentEntities.Add(new TreatmentEntity
                 {TreatmentName = "Bryn retning m. blad", Price = 20, Duration = 10});
             treatmentEntities.Add(new TreatmentEntity
                 {TreatmentName = "Bryn retning m. pincet", Price = 50, Duration = 10});
-            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Bryn farve", Price = 50, Duration = 20});
+            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Bryn farve", Price = 50, Duration = 120});
             treatmentEntities.Add(new TreatmentEntity
-                {TreatmentName = "Bryn farve + retning", Price = 85, Duration = 30});
+                {TreatmentName = "Bryn farve + retning", Price = 85, Duration = 120});
             treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Vipper farvning", Price = 85, Duration = 15});
-            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Bryn + vipper", Price = 150, Duration = 30});
+            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Bryn + vipper", Price = 150, Duration = 60});
             treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Kurbehandling", Price = 150, Duration = 15});
-            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Hverdagsmakeup", Price = 300, Duration = 30});
+            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Hverdagsmakeup", Price = 300, Duration = 60});
             treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Festmakeup", Price = 450, Duration = 30});
-            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Bryllupsmakeup", Price = 550, Duration = 45});
-            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Herreklip", Price = 245, Duration = 30});
-            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Skægklip", Price = 150, Duration = 30});
-            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Glatbarbering", Price = 150, Duration = 30});
+            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Bryllupsmakeup", Price = 550, Duration = 60});
+            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Herreklip", Price = 245, Duration = 60});
+            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Skægklip", Price = 150, Duration = 60});
+            treatmentEntities.Add(new TreatmentEntity {TreatmentName = "Glatbarbering", Price = 150, Duration = 60});
 
             return treatmentEntities.ToArray();
         }
